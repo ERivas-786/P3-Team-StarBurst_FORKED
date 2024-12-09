3a4d8da3911e68fe83c7a7ddc8ac70c1501a951c
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2024-12-09 16:13:40 -0500

    Fixed Correlation Bug

--- graph_generator.py
+++ graph_generator.py
@@ -8,17 +8,22 @@ df = pd.read_csv(path)
 
 path2 = './annual_gold_rate.csv'
 df_2 = pd.read_csv(path2)
-df_2 = df_2.rename(columns={'Date': 'YEAR_NUM'})
+df_2 = df_2.rename(columns={'Date': 'YEAR'})
 
 # Select relevant columns
-df_selected = df[['ESTIMATE', 'YEAR_NUM']]
-df_selected_2 = df_2[['YEAR_NUM', 'USD']]
+df_selected = df[['ESTIMATE', 'YEAR']]
+df_selected_2 = df_2[['YEAR', 'USD']]
 
-merged_df = pd.merge(df, df_2, on='YEAR_NUM')
-final_df = merged_df[['ESTIMATE', 'YEAR_NUM', 'USD']]
+merged_df = pd.merge(df, df_2, on='YEAR')
+final_df = merged_df[['ESTIMATE', 'YEAR', 'USD']]
 
-# final_df.dropna()
-# final_df.reindex(axis = 0)
+final_df.dropna()
+final_df.reindex(axis = 0)
+
+# print(final_df.head())  # View the first few rows
+# print(merged_df.info())  # Inspect data types and non-null counts
+# print(merged_df.describe())  # View summary statistics
+
 
 # Compute the correlation matrix
 correlation_matrix = final_df.corr()
@@ -26,6 +31,7 @@ print(correlation_matrix)
 
 
 # Plot the heatmap
-# sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
-# plt.title("Correlation Heatmap")
-# plt.show()
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+plt.title("Correlation Heatmap")
+plt.show()
+
