e7131b0843a6f371ebd91d9b7733ff4a32b44a25
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2024-12-12 18:02:12 -0500

    More graphical updates

--- suicide_vs_avg_monthly_temp.py
+++ suicide_vs_avg_monthly_temp.py
@@ -36,6 +36,31 @@ print(correlation_matrix)
 # plt.figure(figsize=(8, 6))
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
 plt.title("Suicide vs Temperature")
-# plt.savefig('Obesity_vs_Unemployment_Heatmap.png')
+filePath = './Analysis_results/'
+plt.savefig(filePath + 'Suicide_vs_Monthly_Temp_Heatmap.png')
 plt.show()
 
+
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+# Plot the first line (primary y-axis)
+ax1.plot(merged_df['Year'], merged_df['average_temp'], 'o--', color='blue', label='Dollar Store Search Interest')
+ax1.set_xlabel('Year', fontsize=12)
+ax1.set_ylabel('Average Temp Across the US (F)', fontsize=12, color='blue')
+ax1.tick_params(axis='y', labelcolor='blue')
+
+# Add secondary y-axis for the second line
+ax2 = ax1.twinx()
+ax2.plot(merged_df['Year'], merged_df['Suicide_Rate'], 'o--', color='red', label='Divorce Rate')
+ax2.set_ylabel('Suicide Rate (Per 100,000)', fontsize=12, color='red')
+ax2.tick_params(axis='y', labelcolor='red')
+
+# Add a title and grid
+plt.title('Correlation Between Suicide Rates and Average Yearly Temperature (r=-0.39)', fontsize=14)
+ax1.grid(True, linestyle=':')
+
+# Show the plot
+plt.tight_layout()
+filePath = './Analysis_results/'
+plt.savefig(filePath + 'Suicide_Rate_vs_Temperature.png')
+plt.show()
