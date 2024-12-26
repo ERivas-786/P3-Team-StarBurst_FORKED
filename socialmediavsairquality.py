2be75952efd9808927455011ea0dfbefb62b2341
VitEfimov <135293952+VitEfimov@users.noreply.github.com>
2024-12-26 14:25:59 -0500

    committttt

--- socialmediavsairquality.py
+++ socialmediavsairquality.py
@@ -20,7 +20,18 @@ groupedairqualitydata = airqualitydata.groupby('Month')['CO'].mean()
 # Merge datasets on 'Year'
 merged_data = pd.merge(groupedsocialmediadata, groupedairqualitydata, on="Month")
 
+#re for find pattern
+date_patterns = re.compile(r'(date|year)', re.IGNORECASE)
 
+# rename date|year columns to 'Year/Date'
+def change_column_header(cols):
+    changed = []
+    for item in cols:
+        if date_patterns.search(item):
+            changed.append('Year/Date') 
+        else:
+            changed.append(item)
+    return changed
 # Reset index to convert 'Year' from index to column
 merged_data.reset_index(inplace=True)
 
